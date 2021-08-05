a1a2bfa0c21decbae2c4f884509956a8bfdb2711
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-08-05 10:31:38 +0700

    Add project files.

---
+++ WebAPI_Movie/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_Movie.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAPI_Movie/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_Movie.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
